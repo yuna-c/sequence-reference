2c65b2a6860bc4bcecc4b550960125f2c683769d
yuna-c <yuna744@gmail.com>
2023-11-16 13:00:42 +0900

    1116 이벤트 함수 분리

--- js/main.js
+++ js/main.js
@@ -2,18 +2,11 @@ const frame = document.querySelector('figure');
 const mask = document.querySelector('.mask');
 const countEl = mask.querySelector('span');
 const imgNum = 200;
-let count = 0;
 
 const imgs = createImgs(frame, imgNum);
 imgLoadedCheck(imgs);
-
-frame.addEventListener('mousemove', (e) => {
-	const { pageX } = e;
-	const percent = parseInt((pageX / window.innerWidth) * imgNum);
-
-	imgs.forEach((img) => (img.style.visibility = 'hidden'));
-	imgs[percent].style.visibility = 'visible';
-});
+frame.addEventListener('mousemove', showImg);
+//이벤트 객체가 함수에 호출할 때 인수 전달 하지 않더라도 직계자식함수로 연결되어 있기 때문에 자동으로 연결이 된다(다른 인수 들어갈 시 감싸야댐)
 
 // 동적으로 이미지 생성 함수 분리(돔 생성하자마자 바로 리턴해서 활용가능하도록 처리)
 function createImgs(frame, imgNum, imgName = 'pic') {
@@ -26,6 +19,7 @@ function createImgs(frame, imgNum, imgName = 'pic') {
 
 // 이미지 소스 로딩 유무 체크함수
 function imgLoadedCheck(imgs) {
+	let count = 0;
 	imgs.forEach((img) => {
 		img.addEventListener('error', (e) => {
 			e.currentTarget.setAtrribute('src', 'img/logo.png');
@@ -37,4 +31,13 @@ function imgLoadedCheck(imgs) {
 			if (count === imgNum) mask.remove();
 		});
 	});
+}
+
+// 현재 마우스 포인터 위치에 따른 이미지 보임 함수
+function showImg(e) {
+	const { pageX } = e;
+	const percent = parseInt((pageX / window.innerWidth) * imgNum);
+
+	imgs.forEach((img) => (img.style.visibility = 'hidden'));
+	imgs[percent].style.visibility = 'visible';
 }
