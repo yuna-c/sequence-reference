7befcaf05025e5b92dd7a6a7e2b1f77cce27dfed
yuna-c <yuna744@gmail.com>
2023-11-16 12:07:08 +0900

    1116 이미지 소스 로딩 완료시 마스크 제거

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const frame = document.querySelector('figure');
+const mask = document.querySelector('.mask');
+const countEl = mask.querySelector('span');
 const imgNum = 200;
 let tags = '';
 
@@ -17,8 +19,32 @@ for (let i = 0; i < imgNum; i++) {
 }
 
 frame.innerHTML = tags;
+
+// 동적으로 200개의 DOM이 막 생성된 순간
 const imgs = frame.querySelectorAll('img');
-console.log(imgs);
+// console.log(imgs);
+
+let count = 0;
+
+// 각 동적 생성 이미지 요소를 반복 처리
+imgs.forEach((img) => {
+	img.addEventListener('error', (e) => {
+		console.log('해당 이미지 로딩 실패', e);
+		//특정 이미지의 소스 불러오는데에 실패하면(엑박) 대체이미지 출력
+		e.currentTarget.setAtrribute('src', 'img/logo.png');
+	});
+
+	// 특정 이미지 렌더링 성공시 count값 증가 처리
+	img.addEventListener('load', () => {
+		// 카운트 값을 다시 백분율로 변환해서 로딩화면에 출력
+		countEl.innerText = parseInt((count / imgNum) * 100) + 1;
+		count++;
+		// 카운트 갯수와 전체 이미지 갯수가 동일해지면 (모든 이미지소스가 랜다링 완료시 ) 마스크 제거
+		if (count === imgNum) {
+			mask.remove();
+		}
+	});
+});
 
 frame.addEventListener('mousemove', (e) => {
 	const { pageX } = e;
