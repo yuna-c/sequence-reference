7cb7034325dd183d3804f0ed2ecac6e9a6425963
yuna-c <yuna744@gmail.com>
2023-11-16 11:47:06 +0900

    1116 마우스 위치에 따른 이미지 활성화

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
 const frame = document.querySelector('figure');
 const imgNum = 200;
 let tags = '';
-// console.log(img);
-
-for (let i = 0; i < imgNum; i++) {
-	tags += `<img src = 'img/pic${i}.jpg' title='pic${i}'>`;
-}
-//console.log(tags);
-frame.innerHTML = tags;
-frame.addEventListener('mousemove', (e) => {
-	const { pageX /*pageY*/ } = e;
-	//console.log('x축', pageX);
-	// 백분율 공식 : 현재수치값/ 전체수치값 * 100;
-	const percent = parseInt((pageX / window.innerWidth) * imgNum);
-	console.log(percent);
-	// parseInt('숫자') : 인수로 전달된 값의 소수점 아래를 버리고 정수로 반한
-	// parsFloat('숫자') : 인수로 전달된 값의 소수점까지 포함한 실수로 반환
-});
 
 /*
   1. 동적으로 200개의 img Dom 생성
@@ -27,3 +11,21 @@ frame.addEventListener('mousemove', (e) => {
   6. 이미지 소스가 모두 로딩되기 전 까지는 마스크 화면으로 가려 주면서 로딩 상황을 100분율 출력
   7. 모든 이미지 소스 로딩 완료시 마스크 제거
   */
+
+for (let i = 0; i < imgNum; i++) {
+	tags += `<img src = 'img/pic${i}.jpg' title='pic${i}'>`;
+}
+
+frame.innerHTML = tags;
+const imgs = frame.querySelectorAll('img');
+console.log(imgs);
+
+frame.addEventListener('mousemove', (e) => {
+	const { pageX } = e;
+	const percent = parseInt((pageX / window.innerWidth) * imgNum);
+
+	console.log(percent);
+
+	imgs.forEach((img) => (img.style.visibility = 'hidden'));
+	imgs[percent].style.visibility = 'visible';
+});
